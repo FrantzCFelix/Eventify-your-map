29c85f2120ed69a732cb2461c8fe963f03f1e222
FrantzCFelix <Frantz.C.Felix@gmail.com>
2020-01-08 21:03:29 -0800

    pushed test ajax call to test if api app key will work with github pages link

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 var googleMapsJSApikey = config.google_maps_javaScript_api_key;
 var eventbriteApiKey = config.eventbrite_api_key;
 var eventfulApiKey = config.eventful_api_key;
+
+
+$.ajax({
+    url: "http://api.eventful.com/json/events/search?keywords=music&location=Singapore&app_key=7xvLDCCDGL5MnWLF",
+    method: 'GET'
+   })
+    // After the data comes back from the API
+    .then(function(response) {
+        console.log(response);
+      // Storing an array of results in the results variable
+    });
