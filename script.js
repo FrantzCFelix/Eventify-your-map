8e9a54a2b307a6ef5b28fc3760dec46abea955b5
FrantzCFelix <Frantz.C.Felix@gmail.com>
2020-01-13 23:51:07 -0800

    Made a updateMap function and put that in my ajax response, initMap is now at the top of the script. Made the creation of map markers its own function and took that out of initMap and put in updateMap to prevent unintended behavior. Added code to find user location and set the map to that location.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /// variables needed for API keys
 var googleMapsJSApikey = config.google_maps_javaScript_api_key;
 var ticketmasterApiKey = config.ticket_master_api_key;
+/*************************/
 
 /// variables needed for userCreatedURL
 var startDateTime = "";
@@ -8,37 +9,41 @@ var endDateTime = "";
 var city = "";
 var keyword = "";
 var radius = "";
+/*********************/
+
+/*google maps api vars*/
 var map;
 var seattle = { lat: 47.6062, lng: -122.3321 };
+var tokyo = {lat: 35.6762 , lng: 139.6503};
+var userLocationCoord = {lat: 0 , lng: 0};
 var marker;
 var markerArr;
 
+/**********************/
+ initMap();
 
+//If browser supports geolocation
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      map.setCenter(pos);
+      userLocationCoord.lat = pos.lat;
+      userLocationCoord.lng = pos.lng;
 
-
-function getMapMarkers(ajaxResponse) {
-    var mapMarker = [];
-    for (var i = 0; i < ajaxResponse._embedded.events.length; i++) {
-        var latLngObj = { lat: 0, lng: 0 };
-        latLngObj.lat = parseFloat(ajaxResponse._embedded.events[i]._embedded.venues[0].location.latitude);
-        latLngObj.lng = parseFloat(ajaxResponse._embedded.events[i]._embedded.venues[0].location.longitude);
-        mapMarker.push(latLngObj);
-    }
-   //console.log(mapMarker);
-    return mapMarker;
-}
-
-
-//call back function
-function initMap() {
-    map = new google.maps.Map(document.getElementById('mapImage'), {
-        center: seattle,
-        zoom: 11
+    }, function() {
+      handleLocationError(true);
     });
-    for (var i = 0; i < markerArr.length; i++) {
-        marker = new google.maps.Marker({ position: markerArr[i], map: map });
-    }
-}
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false);
+  }
+
+
+
+
 $("form").on("submit", function(event) {
   event.preventDefault();
   console.log("search button was clicked");
@@ -50,11 +55,16 @@ $("form").on("submit", function(event) {
   keyword = $("#description").val();
   radius = $("#radius").val();
 
-  console.log(startDateTime);
-  console.log(endDateTime);
-  console.log(city);
-  console.log(keyword);
-  console.log(radius);
+  // if(city==="")
+  // {
+  //     city =0; //
+  // }
+
+//   console.log(startDateTime);
+//   console.log(endDateTime);
+//   console.log(city);
+//   console.log(keyword);
+//   console.log(radius);
 
   /// build the URL with user input
   var queryURL =
@@ -81,6 +91,47 @@ $("form").on("submit", function(event) {
     .then(function(response) {
       console.log(response);
       markerArr = getMapMarkers(response);
-      initMap();
+      updateMap();
     });
 });
+
+//takes ticketmaster api data and extracts venue lat&lng
+function getMapMarkers(ajaxResponse) {
+    var mapMarker = [];
+    for (var i = 0; i < ajaxResponse._embedded.events.length; i++) {
+        var latLngObj = { lat: 0, lng: 0 };
+        latLngObj.lat = parseFloat(ajaxResponse._embedded.events[i]._embedded.venues[0].location.latitude);
+        latLngObj.lng = parseFloat(ajaxResponse._embedded.events[i]._embedded.venues[0].location.longitude);
+        mapMarker.push(latLngObj);
+    }
+   //console.log(mapMarker);
+    return mapMarker;
+}
+
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('mapImage'), {
+        center: tokyo,
+        zoom: 11
+    });
+    
+}
+function updateMap() {
+    map = new google.maps.Map(document.getElementById('mapImage'), {
+        center: seattle,
+        zoom: 11
+    });
+    makeMapMarkers();
+    
+}
+function makeMapMarkers(){
+    for (var i = 0; i < markerArr.length; i++) {
+        marker = new google.maps.Marker({ position: markerArr[i], map: map });
+    }
+}
+function handleLocationError(browserHasGeolocation) {
+  console.error(browserHasGeolocation ?
+                      'Error: The Geolocation service failed.' :
+                      'Error: Your browser doesn\'t support geolocation.');
+
+}
