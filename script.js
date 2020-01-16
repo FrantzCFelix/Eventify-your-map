66c00f37e9fc9ea7fe8b3717ffbacb1cee5f0675
Teresa Liu <kuas033@gmail.com>
2020-01-15 21:35:35 -0800

    create landingpage.js

--- script.js
+++ script.js
@@ -9,50 +9,14 @@ var city = "";
 var keyword = "";
 var radius = "";
 
-//typewriter function
-  var str = "<p>Are You Ready To Explore ?</p>",
-  i = 0,
-  isTag,
-  text;
-  
-(function type() {
-  text = str.slice(0, i++);
-  if (text === str) return;
 
-  document.getElementById('typeWriter').innerHTML = text;
 
-  var char = text.slice(-1);
-  if( char === '<' ) isTag = true;
-  if( char === '>' ) isTag = false;
+// console.log(startDateTime);
+// console.log(endDateTime);
+// console.log(city);
+// console.log(keyword);
+// console.log(radius);
 
-  if (isTag) return type();
-  setTimeout(type, 60);
-}()); 
-
-// hand icon function
-
-function hand() {
-  var a;
-  a = document.getElementById("div1");
-  a.innerHTML = "&#xf25a;";
-  setTimeout(function () {
-    a.innerHTML = "&#xf256;";
-  }, 500);
-  setTimeout(function () {
-    a.innerHTML = "&#xf259;";
-  }, 1000);
-  setTimeout(function () {
-    a.innerHTML = "&#xf256;";
-  }, 1500);
-}
-hand();
-setInterval(hand, 2000);
-
-  // console.log(startDateTime);
-  // console.log(endDateTime);
-  // console.log(city);
-  // console.log(keyword);
-  // console.log(radius);
 
   /// build the URL with user input
 
@@ -82,6 +46,7 @@ setInterval(hand, 2000);
     "&city=" +
     city;
   console.log(queryURL);
+  
 
   /// GET request to load data
   $.ajax({
@@ -93,4 +58,3 @@ setInterval(hand, 2000);
       console.log(response);
     });
 });
-
