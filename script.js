5724536982fed8e59f3b6313b603ccc7c8a65bc7
FrantzCFelix <Frantz.C.Felix@gmail.com>
2020-01-16 18:07:55 -0800

    The map now finds the users location by default and moves to thier desired search location

--- script.js
+++ script.js
@@ -14,17 +14,18 @@ var radius = "";
 /*google maps api vars*/
 var map;
 var seattle = { lat: 47.6062, lng: -122.3321 };
-var tokyo = {lat: 35.6762 , lng: 139.6503};
+var tokyo = { lat: 35.6762, lng: 139.6503 };
 var userLocationCoord = tokyo;//set to tokyo to test desired behavior
 var marker;
 var markerArr;
+var cityCoords = tokyo;
 
 /**********************/
 /* Promise declaeration */
-var locationPromise = new Promise(function(resolve, reject){
+var locationPromise = new Promise(function (resolve, reject) {
 
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
@@ -32,44 +33,40 @@ var locationPromise = new Promise(function(resolve, reject){
       userLocationCoord.lat = pos.lat;
       userLocationCoord.lng = pos.lng;
       resolve(userLocationCoord);
-    },function() {
+    }, function () {
       handleLocationError(true);
       console.error("Location Denied");
       reject("promise Location Failed");
     });
   }
-  else
-  {
+  else {
     handleLocationError(false);
-    reject("promise Location Failed");    
+    reject("promise Location Failed");
   }
 
 });
 
 /****************************/
 
-locationPromise.then(function(result){
+locationPromise.then(function (result) {
   //Should try to put this in a promise aysny-await block in search event listener to get city into the queryUrl-FF
   //Right now this relies on the ajax call finishing before the user inputs information and city is equal to the users location by default- FF
   $.ajax({
-    url: "https://maps.googleapis.com/maps/api/geocode/json?latlng="+result.lat+","+result.lng+"&result_type=locality&key="+googleMapsJSApikey,
+    url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + result.lat + "," + result.lng + "&result_type=locality&key=" + googleMapsJSApikey,
     method: "GET"
   })
-  .then(function(response) {
-  
-    city = response.results[0].address_components[0].long_name;
-    $("#location").val(city);
-    console.log("City is equal to: " + response.results[0].address_components[0].long_name);
-   
-  });
- 
-  console.log(result);
-}, function(err){
-  console.log(err);
+    .then(function (response) {
 
+      city = response.results[0].address_components[0].long_name;
+      $("#location").val(city);
+      console.log("City is equal to: " + response.results[0].address_components[0].long_name);
+
+    });
+
+  console.log(result);
 });
 
-$("form").on("submit", function(event) {
+$("form").on("submit", function (event) {
   event.preventDefault();
   console.log("search button was clicked");
 
@@ -77,15 +74,14 @@ $("form").on("submit", function(event) {
   var startDateTime = $("#startDate").val() + "T00:00:00Z";
   var endDateTime = $("#endDate").val() + "T23:59:00Z";
   city = $("#location").val();
-  console.log(city);
   keyword = $("#description").val();
   radius = $("#radius").val();
 
-//   console.log(startDateTime);
-//   console.log(endDateTime);
-//   console.log(city);
-//   console.log(keyword);
-//   console.log(radius);
+  //   console.log(startDateTime);
+  //   console.log(endDateTime);
+  //   console.log(city);
+  //   console.log(keyword);
+  //   console.log(radius);
 
   /// build the URL with user input
   var queryURL =
@@ -103,74 +99,110 @@ $("form").on("submit", function(event) {
     city;
   console.log(queryURL);
 
-  /// GET request to load data
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    // After the data comes back from the API
-    .then(function(response) {
-      console.log(response);
-      markerArr = getMapMarkers(response);
-      updateMap();
-    });
+  /************************** */
+  var cityPromise = new Promise(function (resolve) {//, reject){
+
+
+
+    $.ajax({
+      url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=" + googleMapsJSApikey,
+      method: "GET"
+    })
+      // After the data comes back from the API
+      .then(function (response) {
+        cityCoords = response.results[0].geometry.location;
+        console.log(cityCoords);
+        resolve(response.results[0].geometry.location);
+      });
+
+    // reject("promise Location Failed");
+
+  });
+  console.log(cityPromise);
+  /****************************/
+
+  cityPromise.then(function (result) {
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // After the data comes back from the API
+      .then(function (response) {
+        console.log(response);
+        markerArr = getMapMarkers(response);
+        updateMap();
+      });
+
+      console.log(result);
+  });
+
+  console.log(cityPromise);
+
 });
+
+/******************************* */
+
+
+
+  /// GET request to load data
+ 
+
 
 //takes ticketmaster api data and extracts venue lat&lng
 function getMapMarkers(ajaxResponse) {
-    var mapMarker = [];
-    for (var i = 0; i < ajaxResponse._embedded.events.length; i++) {
-        var latLngObj = { lat: 0, lng: 0 };
-        latLngObj.lat = parseFloat(ajaxResponse._embedded.events[i]._embedded.venues[0].location.latitude);
-        latLngObj.lng = parseFloat(ajaxResponse._embedded.events[i]._embedded.venues[0].location.longitude);
-        mapMarker.push(latLngObj);
-    }
-   //console.log(mapMarker);
-    return mapMarker;
+  var mapMarker = [];
+  for (var i = 0; i < ajaxResponse._embedded.events.length; i++) {
+    var latLngObj = { lat: 0, lng: 0 };
+    latLngObj.lat = parseFloat(ajaxResponse._embedded.events[i]._embedded.venues[0].location.latitude);
+    latLngObj.lng = parseFloat(ajaxResponse._embedded.events[i]._embedded.venues[0].location.longitude);
+    mapMarker.push(latLngObj);
+  }
+  //console.log(mapMarker);
+  return mapMarker;
 }
 
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('mapImage'), {
-        center: tokyo,
-        zoom: 11
+  map = new google.maps.Map(document.getElementById('mapImage'), {
+    center: tokyo,
+    zoom: 11
+  });
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      map.setCenter(pos);
+      userLocationCoord.lat = pos.lat;
+      userLocationCoord.lng = pos.lng;
+    }, function () {
+      handleLocationError(true);
     });
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        map.setCenter(pos);
-        userLocationCoord.lat = pos.lat;
-        userLocationCoord.lng = pos.lng;
-      }, function() {
-        handleLocationError(true);
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false);
-    }
-    
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false);
+  }
+
 }
 function updateMap() {
-    map = new google.maps.Map(document.getElementById('mapImage'), {
-        center: city,
-        zoom: 11
-    });
-    makeMapMarkers();
+  map = new google.maps.Map(document.getElementById('mapImage'), {
+    center: cityCoords,
+    zoom: 11
+  });
+  makeMapMarkers();
 }
-function makeMapMarkers(){
-    for (var i = 0; i < markerArr.length; i++) {
-        marker = new google.maps.Marker({ position: markerArr[i], map: map });
-    }
+function makeMapMarkers() {
+  for (var i = 0; i < markerArr.length; i++) {
+    marker = new google.maps.Marker({ position: markerArr[i], map: map });
+  }
 }
 
 
 
 function handleLocationError(browserHasGeolocation) {
   console.error(browserHasGeolocation ?
-                      'Error: The Geolocation service failed.' :
-                      'Error: Your browser doesn\'t support geolocation.');
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
 
 }
