0a1aa40ecf1f8c707f15e089bb8451db9b998b02
FrantzCFelix <Frantz.C.Felix@gmail.com>
2020-01-17 15:56:08 -0800

    removed some api keys

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 var config = {
-     google_maps_javaScript_api_key: 'AIzaSyA7Wxb7HnT4rc08hFfYfwrurjFkRRLSRkA',
-     eventbrite_api_key: 'ZMAELNSPRSL7AHLI6XYE',
-     eventful_api_key: 'BLG28fCFksPJcL7s',
-     seatgeek_api_key: 'MTM0ODE5Nzh8MTU3ODYyNzMxMC4xMw',
-     seatgeek_secret_key: '04bd3c01f7540c3a57eca09c19a9fd6d0b4d40493611328159180e59fc207f2e',
-     ticket_master_api_key: 'Ox1gFbrU72XDWjgoHudTpdhL9Wmxrc4Y'
- }
+    google_maps_javaScript_api_key: 'AIzaSyA7Wxb7HnT4rc08hFfYfwrurjFkRRLSRkA',
+    ticket_master_api_key: 'Ox1gFbrU72XDWjgoHudTpdhL9Wmxrc4Y'
+}
